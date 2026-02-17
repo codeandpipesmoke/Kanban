a7b705fb3bb38812d8603f18c526caa2052ff8e1
Jeff Shoemaker <zsfoto@gmail.com>
2026-02-17 10:44:50 +0100

    Configure::write('DebugKit.forceEnable', function() {...}

--- src/Application.php
+++ src/Application.php
@@ -52,6 +52,19 @@ class Application extends BaseApplication
 
         // By default, does not allow fallback classes.
         FactoryLocator::add('Table', (new TableLocator())->allowFallbackClass(false));
+
+		//Configure::write('DebugKit.forceEnable', true);
+		Configure::write('DebugKit.forceEnable', function() {
+			if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+				$ip = $_SERVER['HTTP_CLIENT_IP'];
+			} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+				$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			} else {
+				$ip = $_SERVER['REMOTE_ADDR'];
+			}
+			return $ip === '127.0.0.1';
+			//return false;
+		});
     }
 
     /**
